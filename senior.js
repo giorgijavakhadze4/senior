bc3152d29d9bce29a99f2e81ae9f89e16103bb8e
giorgijavakhadze4 <giorgijavaxadze707@gmail.com>
2025-03-27 23:25:41 +0400

    forth change

--- senior.js
+++ senior.js
@@ -5,3 +5,8 @@ console.log("we are the craziest programmers!!!")
 console.log("It's raining today")
 
 console.log("It's cold")
+
+function change(){
+    console.log(":It's cold")
+    return 100
+}
